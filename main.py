0dd4ad30cc539db8c08cce8fd240ec57cec1e4d2
andre <andre@zuplae.com>
2023-05-27 14:52:34 -0300

    Subindo codigo flask

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+from pessoa import Pessoa
 
+# Importacao e ultilizacao da nossa clase pessoa
+pessoa1 = Pessoa("Luciano", "18", "1.80")
+pessoa2 = Pessoa("Jose", "35", "1.81")
+pessoa3 = Pessoa("Marcelinho", "19", "1.60")
+pessoa4 = Pessoa("Andre", "25", "1.80")
+
+lista = [pessoa1, pessoa2, pessoa3, pessoa4]
+
+
+# tudo que depende do nosso framework, deve estar abaixo do objeto da classe Flask
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    return render_template('index.html')
+    return render_template('index.html', titulo = 'Home', pessoas = lista)
 
+@app.route('/novo')
+def criar():
+    return render_template('novo.html', titulo = "Formulario de cadastro")
 
 app.run(debug=True)
